f4bebf1f27ca63b7df9e5953c99bb0f77c1a4129
SnakyMz <Soban.syeed@outlook.com>
2023-11-02 20:12:20 +0530

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,13 +2,13 @@
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
-second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 0)
+second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.', posts_counter: 0)
 
-first_post1 = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
-first_post2 = Post.create(author: first_user, title: 'Bye', text: 'Best of luck with rails')
-second_post1 = Post.create(author: second_user, title: 'Hello', text: 'My first post')
-second_post2 = Post.create(author: second_user, title: 'Hello', text: 'Happy coding')
+first_post1 = Post.create(author: first_user, title: 'Hello', text: 'This is my first post', comments_counter: 0, likes_counter: 0)
+first_post2 = Post.create(author: first_user, title: 'Bye', text: 'Best of luck with rails', comments_counter: 0, likes_counter: 0)
+second_post1 = Post.create(author: second_user, title: 'Hello', text: 'My first post', comments_counter: 0, likes_counter: 0)
+second_post2 = Post.create(author: second_user, title: 'Hello', text: 'Happy coding', comments_counter: 0, likes_counter: 0)
 
 Comment.create(post: first_post1, user: second_user, text: 'Hi Tom!' )
 Comment.create(post: first_post1, user: first_user, text: 'Hi Lilly!' )
