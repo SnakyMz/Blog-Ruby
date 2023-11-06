f9cb7377b03fa6cf88b44679b2963229e2075d47
SnakyMz <Soban.syeed@outlook.com>
2023-11-07 05:18:23 +0530

    Add user show spec

--- spec/system/users_index_spec.rb
+++ spec/system/users_index_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe "Users", type: :system do
   before do
     driven_by(:rack_test)
-    @user = User.create(name: 'TestBot', photo: 'test photo', bio: 'Tester', posts_counter: 4)
+    @user = User.create(name: 'Albert', photo: 'test photo', bio: 'Tester', posts_counter: 4)
   end
 
   it 'Should render user name' do
     visit root_path
-    expect(page).to have_content('TestBot')
+    expect(page).to have_content('Albert')
   end
 
   it 'Should render user photo' do
@@ -23,7 +23,7 @@ RSpec.describe "Users", type: :system do
 
   it 'Should click the link with the name of the user' do
     visit root_path
-    click_on('TestBot')
+    click_on('Albert')
     expect(page).to have_current_path(user_path(id: @user.id))
   end
 end
