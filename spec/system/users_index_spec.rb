821f811db0952dd272307595406d26a94b0eba63
SnakyMz <Soban.syeed@outlook.com>
2023-11-07 20:16:50 +0530

    Add post index spec

--- spec/system/users_index_spec.rb
+++ spec/system/users_index_spec.rb
@@ -4,25 +4,22 @@ RSpec.describe "Users", type: :system do
   before do
     driven_by(:rack_test)
     @user = User.create(name: 'Albert', photo: 'test photo', bio: 'Tester', posts_counter: 4)
+    visit root_path
   end
 
   it 'Should render user name' do
-    visit root_path
     expect(page).to have_content('Albert')
   end
 
   it 'Should render user photo' do
-    visit root_path
     expect(page.html).to include('userPhoto')
   end
 
   it 'Should render number of posts' do
-    visit root_path
     expect(page).to have_content('Number of posts: 4')
   end
 
   it 'Should click the link with the name of the user' do
-    visit root_path
     click_on('Albert')
     expect(page).to have_current_path(user_path(id: @user.id))
   end
