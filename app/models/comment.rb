56e12d314a5c9d3e521013f23b2d4ccd35b8b901
Rysth <johnpalacios.t@gmail.com>
2023-11-09 12:26:27 -0500

    Add increase and decrease comments methods in Comment model.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,16 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  after_save :update_comments_counter
+  after_save :increase_comments_counter
+  after_destroy :decrease_comments_counter
 
   private
-
-  def update_comments_counter
+  
+  def increase_comments_counter
     post.increment!(:comments_counter)
+  end
+
+  def decrease_comments_counter
+    post.decrement!(:comments_counter)
   end
 end
